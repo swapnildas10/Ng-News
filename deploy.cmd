f1a74dfa79b5ff8ee5f4a1437020eb8bcf7b5d7b
swapnildas10 <swapnildas@github.com>
2018-04-28 16:30:58 -0700

    deployment modifications

--- deploy.cmd
+++ deploy.cmd
@@ -2,7 +2,7 @@
 
 :: ----------------------
 :: KUDU Deployment Script
-:: Version: 1.0.15
+:: Version: 1.0.17
 :: ----------------------
 
 :: Prerequisites
@@ -93,10 +93,10 @@ call :SelectNodeVersion
 
 :: 2. Install npm packages
 IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
-  pushd "%DEPLOYMENT_SOURCE%"
-  call :ExecuteCmd !NPM_CMD! install
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
+ pushd "%DEPLOYMENT_SOURCE%"
+ call :ExecuteCmd !NPM_CMD! install
+ IF !ERRORLEVEL! NEQ 0 goto error
+ popd
 )
 
 :: 3. Angular Prod Build
@@ -110,17 +110,17 @@ popd
 
 :: 4. Copy Web.config
 IF EXIST "%DEPLOYMENT_SOURCE%\web.config" (
-  pushd "%DEPLOYMENT_SOURCE%"
- :: the next line is optional to fix 404 error see section #8
-  call :ExecuteCmd cp web.config wwwroot\
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
+ pushd "%DEPLOYMENT_SOURCE%"
+:: the next line is optional to fix 404 error see section #8
+ call :ExecuteCmd cp web.config dist\
+ IF !ERRORLEVEL! NEQ 0 goto error
+ popd
 )
 
 :: 5. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
+ call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+ IF !ERRORLEVEL! NEQ 0 goto error
 )
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::

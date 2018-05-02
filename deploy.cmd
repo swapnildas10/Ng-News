fd14c2709b80982cb2601b17c243bebb863643f8
swapnildas10 <swapnildas@github.com>
2018-05-01 19:54:20 -0700

    deployment modifications

--- deploy.cmd
+++ deploy.cmd
@@ -100,7 +100,8 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install
+  echo npm install --production
+  call :ExecuteCmd npm install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )

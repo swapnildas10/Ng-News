323b1470ba5d02d1e02d3ee0ae847aeff86eaad9
swapnildas10 <swapnildas@github.com>
2018-05-10 23:17:51 -0700

    deploy cmd

--- deploy.cmd
+++ deploy.cmd
@@ -80,16 +80,17 @@ IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
 )
 
 goto :EOF
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Deployment
 :: ----------
- 
+
 :Deployment
 echo Handling node.js deployment.
- 
+call :ExecuteCmd mkdir "%DEPLOYMENT_SOURCE%\lib"
+call :ExecuteCmd copy NUL "%DEPLOYMENT_SOURCE%\lib\app.js"
 :: 1. Select node version
 call :SelectNodeVersion
- 
+
 :: 2. Install npm packages
 IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   pushd "%DEPLOYMENT_SOURCE%"
@@ -97,17 +98,19 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
- 
+
 :: 3. Angular Prod Build
 IF EXIST "%DEPLOYMENT_SOURCE%/.angular-cli.json" (
 echo Building App in %DEPLOYMENT_SOURCE%…
 pushd "%DEPLOYMENT_SOURCE%"
 call :ExecuteCmd !NPM_CMD! run build
+:: If the above command fails comment above and uncomment below one
+:: call ./node_modules/.bin/ng build –prod
 IF !ERRORLEVEL! NEQ 0 goto error
 popd
 )
- 
-:: 5. KuduSync
+
+:: 4. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error

3a7556b1f0bafa8909698ab49df0331014a2a254
swapnildas10 <swapnildas@github.com>
2018-05-12 23:47:03 -0700

    dp

--- deploy.cmd
+++ deploy.cmd
@@ -42,7 +42,6 @@ echo =======  Installing npm  devDependancy packages: Starting at %TIME% =======
 echo "%DEPLOYMENT_SOURCE%\package.json"
 IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   pushd "%DEPLOYMENT_SOURCE%"
-  call npm cache clean --force
   call npm install --save
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
